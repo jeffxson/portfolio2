2b66eb9f1a62cf15f51bafabebe21f37a0ee7130
your_username <your_email_address@example.com>
2021-09-02 08:20:33 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter , Route } from 'react-router-dom'
 import Homescreen from './profolio/Homescreen'
 import AboutScreen from './profolio/AboutScreen'
 import Resume from './profolio/Resume'
@@ -10,19 +10,16 @@ import Header from './profolio/container/Header'
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
      <Header />
     <main className='py'> 
      <Route path= '/' component= {Homescreen} exact /> 
      <Route path= '/about' component= {AboutScreen}  /> 
      <Route path= '/resume' component= {Resume}  /> 
      <Route path= '/contact' component= {ContactUs}  /> 
-     <AboutScreen /> 
-     <Resume /> 
-     <ContactUs /> 
-    
+   
     </main>
-  </Router>
+  </BrowserRouter>
   )
 }
 
